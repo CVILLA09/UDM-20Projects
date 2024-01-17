92571d1a44cd273a7788d675731aae0130888a20
Cristian Villa <c.villavirgen@gmail.com>
2024-01-17 04:14:47 -0600

    Extend the js code

--- Project11-SliderColorBG/script.js
+++ Project11-SliderColorBG/script.js
@@ -6,27 +6,47 @@ function updateStyle() {
 
     const value = Number(number.innerText);
 
-    if (value >= 90) {
+    if (value >= 95) {
         applyStyles('white', '#FF0000');
+    } else if (value >= 90) {
+        applyStyles('white', '#FF6600');
+    } else if (value >= 85) {
+        applyStyles('yellow', '#FFCC00');
     } else if (value >= 80) {
-        applyStyles('#111', '#FF6600');
+        applyStyles('yellow', '#99FF00');
+    } else if (value >= 75) {
+        applyStyles('yellow', '#00FF66');
     } else if (value >= 70) {
-        applyStyles('#222', '#FFCC00');
+        applyStyles('gray', '#00FFFF');
+    } else if (value >= 65) {
+        applyStyles('gray', '#0066FF');
     } else if (value >= 60) {
-        applyStyles('#333', '#99FF00');
+        applyStyles('gray', '#6600FF');
+    } else if (value >= 55) {
+        applyStyles('black', '#CC00FF');
     } else if (value >= 50) {
-        applyStyles('#444', '#00FF66');
+        applyStyles('black', '#FF00CC');
+    } else if (value >= 45) {
+        applyStyles('black', '#FF0099');
     } else if (value >= 40) {
-        applyStyles('#555', '#00FFFF');
+        applyStyles('purple', '#FF0066');
+    } else if (value >= 35) {
+        applyStyles('purple', '#FF0033');
     } else if (value >= 30) {
-        applyStyles('#666', '#0066FF');
+        applyStyles('purple', '#FF3300');
+    } else if (value >= 25) {
+        applyStyles('orange', '#FF6600');
     } else if (value >= 20) {
-        applyStyles('#777', '#6600FF');
+        applyStyles('orange', '#FF9900');
+    } else if (value >= 15) {
+        applyStyles('orange', '#FFCC00');
     } else if (value >= 10) {
-        applyStyles('#888', '#CC00FF');
+        applyStyles('red', '#FFFF00');
+    } else if (value >= 5) {
+        applyStyles('red', '#CCFF00');
     } else if (value === 0) {
         applyStyles('red', '#FF00CC');
-    }    
+    }           
 
 }
 
